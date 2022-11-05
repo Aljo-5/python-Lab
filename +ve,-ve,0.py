68f8bc2bbc5a5b476fad25f125b43178257e36c5
Aljo-5 <99000107+Aljo-5@users.noreply.github.com>
2022-11-05 09:34:44 +0530

    Add files via upload

---
+++ +ve,-ve,0.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=int(input("Enter the number"))
+if(a>0):
+    print("It is a positive number")
+elif(a<0):
+    print("It is a negative number")
+else:
+    print("number is zero")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ even or odd.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a=int(input("Enter the number"))
+if(a%2==0):
+    print("It is a even number")
+else:
+    print("It is a odd number")
+
+
+# In[ ]:
+
+
+
+
