63b4737ac6308809088815cbc7f8095aee043029
Aljo-5 <99000107+Aljo-5@users.noreply.github.com>
2022-11-05 09:49:06 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+n=int(input("Enter the number:"))
+fact=1
+for i in range(1, n+1):
+    fact=fact*i
+    
+print("Factorial is:",fact)   
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
